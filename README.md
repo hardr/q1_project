565b9f30d7475fce7acd4bf054d6934ba62c716a
Ryan Harings <ryanharings@hotmail.com>
2016-08-10 10:19:22 -0600

    map style and quest updates

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Repository for g30 Q1 projects.
+Ryan Harings personal repo for g30 Q1 projects.
+
+RPG game taking place in the real world.. ish.
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,11 @@ $(document).ready(function() {
     $('#welcomeModal').show();
     //sets character screen as default view
     $('#map').show();
+    google.maps.event.trigger(map, "resize");
+  });
+
+  $(window).resize(function() {
+    google.maps.event.trigger(map, "resize");
   });
 
   var warriorClass, warriorName, warriorColor;
