b3010beaa67905810a36b825eb55f1c316db938d
Ryan Harings <ryanharings@hotmail.com>
2016-08-12 10:22:55 -0600

    fluffed the fight sequence, final commit before pres

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,11 +9,11 @@ $(document).ready(function() {
     $('#welcomeModal').show();
     //sets character screen as default view
     $('#map').show();
-    google.maps.event.trigger(map, "resize");
+    google.maps.event.trigger(map, 'resize');
   });
 
   $(window).resize(function() {
-    google.maps.event.trigger(map, "resize");
+    google.maps.event.trigger(map, 'resize');
   });
 
   var warriorClass, warriorName, warriorColor;
@@ -23,7 +23,6 @@ $(document).ready(function() {
     $('.warClass').removeClass('classSelect');
     $(this).addClass('classSelect');
     warriorClass = $(this).attr('id');
-    console.log(warriorClass);
   });
 
   //returns image to normal size, but keeps selection
@@ -37,19 +36,68 @@ $(document).ready(function() {
     //assign inputs
     warriorName = $('#warName').val();
     warriorColor = $('#warColor').val();
-    console.log(warriorName);
-    console.log(warriorColor);
     //check inputs are selected
-    if (warriorClass === undefined || warriorName === undefined || warriorColor === undefined) {
-      //error message if incomplete
-      $('.incomplete').text('Complete your warrior');
-      $('.incomplete').fadeIn(500).delay(2000).fadeOut(1000);
-    } else {
+    if (warriorClass !== undefined && warriorName !== null && warriorColor !== null) {
       $('#charImage').append('<img src="images/' + warriorClass + '.jpeg"</img>');
       //hide modal if complete
       $('#charModal').hide();
+      $('#tavCombatModal').fadeIn(1);
+      $('.intense').delay(10000).fadeOut(1000);
+      $('.fatality').hide().delay(11000).fadeIn(1).delay(10000);
+    } else {
+      //error message if incomplete
+      $('.incomplete').text('Complete your warrior');
+      $('.incomplete').fadeIn(500).delay(2000).fadeOut(1000);
     }
   });
+
+  $('.fatality').on('click', function(e) {
+    e.preventDefault();
+    $('#tavCombatModal').hide();
+  })
+
+  $('#tavern').on('click', '.tavBtn', function() {
+    $(this).append('<div class="questAccept">I\'ll check it out!</div>');
+  });
+
+  // //Fight animation
+  // $('#ryu').append('<img src="http://i.imgur.com/90Mmdcm.png">');
+  //
+  // // change the img src on hover to the animated gif of Ryu
+  // // url: http://i.imgur.com/nTj3Fxx.gif
+  // // when the user 'unhovers' change back to static Ryu
+  // $('#ryu > img').hover(function() {
+  //   this.src = 'http://i.imgur.com/nTj3Fxx.gif';
+  // }, function() {
+  //   this.src = 'http://i.imgur.com/90Mmdcm.png';
+  // });
+  //
+  // // when a user clicks, change Ryu's stance
+  // // url: http://i.imgur.com/Rfj0a80.png
+  // $('#ryu > img').on('mousedown', function() {
+  //   this.src = 'http://i.imgur.com/Rfj0a80.png';
+  // });
+  //
+  // // add the Hadouken!
+  // // url: http://i.imgur.com/oTyQRvX.gif
+  // $('#ryu > img').on('mousedown', function() {
+  //   $('#ryu').append('<img class="demo-hadouken" src="http://i.imgur.com/oTyQRvX.gif">');
+  // });
+  //
+  // // animate that Hadouken
+  // $('#ryu > img').on('mousedown', function() {
+  //   $('.demo-hadouken').animate({
+  //     'margin-left': '600px'
+  //   }, 4000, 'swing', function() {
+  //     this.remove();
+  //   });
+  // });
+  //
+  // // let Ryu relax
+  // // url: http://i.imgur.com/90Mmdcm.png
+  // $('#ryu > img').on('mouseup', function() {
+  //   this.src = 'http://i.imgur.com/90Mmdcm.png';
+  // });
 
   //close modal when clicking outside window
   // $(window).on('click', function(e) {
--- src/scripts/map.js
+++ src/scripts/map.js
@@ -10,16 +10,26 @@
 //     styles: mapStyles
 //   });
 // }
+
+$('form').on('click', '#modalClose', function(e) {
+  e.preventDefault();
+  //assign inputs
+  warColor = $('#warColor').val();
+  charMarkerCenter();
+});
+
 var map;
 var myPos;
 var marker;
 var infoWindow;
+var warColor = 'black';
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('mapCont'), {
     center: {lat: 39.739236, lng: -104.990251},
-    zoom: 15,
+    zoom: 16,
     disableDefaultUI: true,
+    scrollwheel: false,
     styles: mapStyles
   });
   infoWindow = new google.maps.InfoWindow({map: map});
@@ -34,9 +44,9 @@ function initMap() {
       myPos = pos;
 
       infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      infoWindow.setContent('Let\'s go ninja some shit!');
       map.setCenter(pos);
-      createMarker(map, markerTavern(), 'tavern');
+      createMarker(map, markerTavern);
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
@@ -46,35 +56,79 @@ function initMap() {
   }
 }
 
+setInterval(function() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var newPos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+    var googleMyPos = new google.maps.LatLng(myPos.lat, myPos.lng);
+    var googleNewPos = new google.maps.LatLng(newPos.lat, newPos.lng);
+
+    if (google.maps.geometry.spherical.computeDistanceBetween(googleMyPos, googleNewPos) > 15) {
+      myPos = newPos;
+      map.setCenter(myPos);
+
+    }
+  });
+}, 3000);
+
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
 }
 
-function createMarker(addMap, markerLoc, type) {
+function charMarkerCenter() {
+  var charMarker = new google.maps.Marker({
+    map: map,
+    position: myPos,
+    icon: {
+      url: './icons/ninja-portrait-' + warColor + '.svg',
+      scaledSize: new google.maps.Size(45, 45),
+      origin: new google.maps.Point(0,0), // origin
+      anchor: new google.maps.Point(0, 0) // anchor
+    }
+  });
+  map.setCenter(myPos);
+}
+
+function createMarker(addMap, markerLoc) {
+  var count = 0;
+
   marker = new google.maps.Marker({
     map: addMap,
-    position: markerLoc,
+    position: markerLoc.pos,
     icon: {
-      url: '../icons/' + type + '.svg',
-      scaledSize: new google.maps.Size(40, 40),
+      url: './icons/' + markerLoc.type + '.svg',
+      scaledSize: new google.maps.Size(45, 45),
       origin: new google.maps.Point(0,0), // origin
       anchor: new google.maps.Point(0, 0) // anchor
     }
   });
   marker.addListener('mouseover', function() {
     // map.setCenter(marker.getPosition());
-    infoWindow.setPosition(markerLoc);
-    infoWindow.setContent(type);
+    infoWindow.setPosition(markerLoc.pos);
+    infoWindow.setContent(markerLoc.type);
   });
+  // map.addListener('center_changed', function() {
+  //   var googleMyPos = new google.maps.LatLng(myPos.lat, myPos.lng);
+  //   var googleMarkerPos = new google.maps.LatLng(markerLoc.pos.lat, markerLoc.pos.lng);
+  //
+  //   if (google.maps.geometry.spherical.computeDistanceBetween(googleMyPos, googleMarkerPos) < 20 && count < 1) {
+  //     alert('You have arrived!');
+  //     count++;
+  //   }
+  // });
+
   $('<li />')
-    .html('../icons/' + type + '.svg')
+    .html('<img src="./icons/' + markerLoc.type + '.svg" style="width:20px;height:20px;">')
     .click(function(e) {
-      map.panTo(markerLoc);
+      map.panTo(markerLoc.pos);
     })
     .appendTo('#mapList');
+  $('<div />')
+    .html(markerLoc.desc)
+    .appendTo('#quest');
   // marker.addListener('mouseout', function() {
   //   // map.setCenter(marker.getPosition());
   //   infoWindow.setMap(null);
@@ -85,26 +139,42 @@ function createMarker(addMap, markerLoc, type) {
   // });
 }
 
-function markerTavern() {
-  return { lat: 39.733603, lng: -104.992688 };
-}
+var markerTavern = {
+  pos: { lat: 39.733603, lng: -104.992688 },
+  desc: 'The Tavern:<br>A safe place to drown your sorrows, and find quests.. until a drunk terrorist drunkenly terrorizes it.',
+  type: 'tavern'
+};
 
-function markerCreek() {
-  return { lat: 39.733300, lng: -104.993383};
-}
+var markerCreek = {
+  pos: { lat: 39.733300, lng: -104.993383},
+  desc: 'Halt, thieves!:<br>Thieves have robbed the tavern and are hiding out near the creek',
+  type: 'quest'
+};
 
-function markerPark() {
-  return { lat: 39.730562, lng: -104.992311};
-}
+var markerPark = {
+  pos: { lat: 39.730562, lng: -104.992311},
+  desc: 'Skynet:<br>Goblins are hacking into the PlaceCage website. This could mean the end of the world if they aren\'t stopped',
+  type: 'quest'
+};
+
+// var charMarker = new google.maps.Marker({
+//   map: map,
+//   position: myPos,
+//   icon: {
+//     url: './icons/ninja-portrait-' + warColor + '.svg',
+//     scaledSize: new google.maps.Size(30, 30),
+//     origin: new google.maps.Point(0,0), // origin
+//     anchor: new google.maps.Point(0, 0) // anchor
+//   }
+// });
 
 $('#barQuest').on('click', function() {
-  createMarker(map, markerCreek(), 'quest');
+  createMarker(map, markerCreek);
 });
 
 $('#wandQuest').on('click', function() {
-  createMarker(map, markerPark(), 'quest');
+  createMarker(map, markerPark);
 });
-
 
 // function updateMap() {
 //   $.ajax({
@@ -118,46 +188,45 @@ $('#wandQuest').on('click', function() {
 
 var mapStyles = [
   {
-    "featureType": "landscape.natural.terrain",
-    "elementType": "geometry.fill",
-    "stylers": [
-      { "hue": "#ffee00" },
-      { "visibility": "simplified" },
-      { "color": "#13611A" }
+    featureType: 'landscape.natural.terrain',
+    elementType: 'geometry.fill',
+    stylers: [
+      { hue: '#ffee00' },
+      { visibility: 'simplified' },
+      { color: '#13611A' }
     ]
   },{
-    "featureType": "road",
-    "elementType": "geometry.fill",
-    "stylers": [
-      { "color": "#E0A869" },
-      { "visibility": "on" }
+    featureType: 'road',
+    elementType: 'geometry.fill',
+    stylers: [
+      { color: '#E0A869' },
+      { visibility: 'on' }
     ]
   },{
-    "featureType": "road",
-    "elementType": "labels",
-    "stylers": [
-      { "visibility": "off" }
+    featureType: 'road',
+    elementType: 'labels',
+    stylers: [
+      { visibility: 'off' }
     ]
   },{
-    "featureType": "landscape.man_made",
-    "stylers": [
-      { "visibility": "simplified" },
-      { "color": "#938a3b" },
-      { "lightness": -13 }
+    featureType: 'landscape.man_made',
+    stylers: [
+      { visibility: 'simplified' },
+      { color: '#938a3b' },
+      { lightness: -13 }
     ]
   },{
-    "featureType": "poi",
-    "elementType": "labels",
-    "stylers": [
-      { "visibility": "off" }
+    featureType: 'poi',
+    elementType: 'labels',
+    stylers: [
+      { visibility: 'off' }
     ]
   },{
-    "featureType": "water",
-    "elementType": "geometry.fill",
-    "stylers": [
-      { "visibility": "on" },
-      { "color": "#4480c8" }
+    featureType: 'water',
+    elementType: 'geometry.fill',
+    stylers: [
+      { visibility: 'on' },
+      { color: '#4480c8' }
     ]
-  },{
   }
 ];
